ebc474385e3d86cdf1768ab5bedcb311374694c0
Aubrey Corsetti <aubreylcorsetti@icloud.com>
2022-12-28 18:08:06 -0800

    had to refactor ALL of my code for game_logic and added game.py.

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -3,71 +3,109 @@ from collections import Counter
 
 
 class GameLogic:
-
     @staticmethod
     def roll_dice(num_dice):
-        """
-
-        :param num_dice:
-        :return: tuple with random values between 1 and 6
-        """
-        return tuple(randint(1, 1) for _ in range(0, num_dice))
+        values = []
+        for i in range(num_dice):
+            value = randint(1, 6)
+            values.append(value)
+        return tuple(values)
 
     @staticmethod
-    def calculate_score(roll):
-        """
-              param: a tuple of integers that represent a dice roll.
-              :return: an integer representing the roll’s score according to rules of game.
-              """
+    def calculate_score(values):
+        score = 0
+        counts = Counter(values)
+        pair_counter = 0
 
-        total = 0
-        counted_dice = Counter(roll).most_common()
-        print(counted_dice)
-
-        if not counted_dice:
-            return total
-
-        if (counted_dice[0][1]) >= 3:
-            if counted_dice[0][0] != 1:
-                total += counted_dice[0][0] * 100 * (counted_dice[0][1] - 2)
-                if counted_dice[0][1] == 6:
-                    return total
-                counted_dice = counted_dice[1:]
-                print(counted_dice)
-            else:
-                total += 1000 * (counted_dice[0][1] - 2)
-                if counted_dice[0][1] == 6:
-                    return total
-                counted_dice = counted_dice[1:]
-
-            if len(counted_dice) == 1:
-                if counted_dice[0][1] == 3:
-                    if counted_dice[0][0] != 1:
-                        total += counted_dice[0][0] * 100
-                        return total
-                    else:
-                        total += 1000
-                        return total
-
-        if len(counted_dice) == 6:
-            total += 1500
-            return total
-
-        if len(counted_dice) == 3:
-            if counted_dice[2][1] == 2:
-                total += 1500
-                return total
-
-        else:
-            for dice in counted_dice:
-                if dice[0] == 5:
-                    total += dice[1] * 50
-            for dice in counted_dice:
-                if dice[0] == 1:
-                    total += dice[1] * 100
-
-        print("Total Score", total)
-        return total
+        for die in counts:
+            if counts[die] == 2:
+                pair_counter += 1
+            if pair_counter < 3 and len(counts) < 6:
+                if die == 1:
+                    if counts[die] == 1:
+                        score += 100
+                    elif counts[die] == 2:
+                        score += 200
+                    elif counts[die] == 3:
+                        score += 1000
+                    elif counts[die] == 4:
+                        score += 2000
+                    elif counts[die] == 5:
+                        score += 3000
+                    elif counts[die] == 6:
+                        score += 4000
+                if die == 2:
+                    if counts[die] == 1:
+                        score += 0
+                    elif counts[die] == 2:
+                        score += 0
+                    elif counts[die] == 3:
+                        score += 200
+                    elif counts[die] == 4:
+                        score += 400
+                    elif counts[die] == 5:
+                        score += 600
+                    elif counts[die] == 6:
+                        score += 800
+                if die == 3:
+                    if counts[die] == 1:
+                        score += 0
+                    elif counts[die] == 2:
+                        score += 0
+                    elif counts[die] == 3:
+                        score += 300
+                    elif counts[die] == 4:
+                        score += 600
+                    elif counts[die] == 5:
+                        score += 900
+                    elif counts[die] == 6:
+                        score += 1200
+                if die == 4:
+                    if counts[die] == 1:
+                        score += 0
+                    elif counts[die] == 2:
+                        score += 0
+                    elif counts[die] == 3:
+                        score += 400
+                    elif counts[die] == 4:
+                        score += 800
+                    elif counts[die] == 5:
+                        score += 1200
+                    elif counts[die] == 6:
+                        score += 1600
+                if die == 5:
+                    if counts[die] == 1:
+                        score += 50
+                    elif counts[die] == 2:
+                        score += 100
+                    elif counts[die] == 3:
+                        score += 500
+                    elif counts[die] == 4:
+                        score += 1000
+                    elif counts[die] == 5:
+                        score += 1500
+                    elif counts[die] == 6:
+                        score += 2000
+                if die == 6:
+                    if counts[die] == 1:
+                        score += 0
+                    elif counts[die] == 2:
+                        score += 0
+                    elif counts[die] == 3:
+                        score += 600
+                    elif counts[die] == 4:
+                        score += 1200
+                    elif counts[die] == 5:
+                        score += 1800
+                    elif counts[die] == 6:
+                        score += 2400
+        if pair_counter == 1 and counts[die] == 3:
+            score = 1500
+        if pair_counter == 3:
+            score = 1500
+        if len(counts) == 6:
+            score = 1500
+        return score
 
 
 
